ce6fbf1622c50f305ec06bc5496c01e8788f76f8
lixingping <lixingping233@gmail.com>
2020-05-05 20:51:30 +0800

    Init commit.

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <div class="search">
+        <input placeholder="请输入....." type="search"/>
+        <button :class="{jello: active}" @click="handleSearch">搜索</button>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        active: false
+      }
+    },
+    methods: {
+      handleSearch() {
+        this.active = true;
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+  .nav {
+    height: 200px;
+    padding-top: 50px;
+  }
+
+  .jello {
+    animation: jello 2s ease-in-out;
+  }
+
+  .search {
+    text-align: center;
+
+    button {
+      display: inline-block;
+      line-height: 1;
+      white-space: nowrap;
+      cursor: pointer;
+      background: #3A71A8;
+      color: white;
+      -webkit-appearance: none;
+      text-align: center;
+      box-sizing: border-box;
+      outline: none;
+      border: none;
+      margin: 0;
+      padding: 0;
+      font-size: 14px;
+      border-radius: 4px;
+
+      width: 80px;
+      height: 40px;
+
+      &:hover {
+        opacity: .8;
+      }
+    }
+
+    input[type='search'] {
+      width: 300px;
+      min-height: 40px;
+
+      padding: 8px;
+
+      outline: none;
+      border: 1px solid #c8c9cc;
+      font-size: 14px;
+      border-radius: 4px;
+
+      &:focus {
+        border: 1px solid #177EE6;
+      }
+    }
+  }
+
+  ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+    text-indent: 8px;
+    color: #969799;
+  }
+
+  ::-moz-placeholder { /* Firefox 19+ */
+    text-indent: 8px;
+    color: #969799;
+  }
+
+  :-ms-input-placeholder { /* IE 10+ */
+    text-indent: 8px;
+    color: #969799;
+  }
+
+  :-moz-placeholder { /* Firefox 18- */
+    text-indent: 8px;
+    color: #969799;
+  }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="app-container">
+    <div class="nav-list">
+
+      <div class="nav-item" v-for="i in 12">
+        <div class="nav-box" @click="openLink('https://www.baidu.com')">
+          <img src="@/assets/links/baidu.png">
+
+          <div class="nav-box-title text">百度</div>
+          <p class="nav-box-desc text">百度一下，你就知道</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import openWindow from "@/utils/openWindow";
+
+  export default {
+    name: 'Home',
+    data() {
+      return {}
+    },
+    methods: {
+      openLink(link) {
+        openWindow(link, '百度', 600, 700)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+  @import '../styles/shadow.scss';
+
+  .nav-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .nav-item {
+      padding: 10px;
+    }
+
+    img {
+      width: 150px;
+    }
+
+    .nav-box {
+      cursor: pointer;
+      padding: 10px;
+      position: relative;
+
+      text-align: center;
+      background-color: white;
+      width: 200px;
+      border-radius: 4px;
+
+      .nav-box-title {
+        margin-top: 10px;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 34px;
+        color: #323233;
+      }
+
+      .nav-box-desc {
+        font-size: 12px;
+        line-height: 20px;
+        color: #969799;
+      }
+
+      .text:hover {
+        color: #177EE6;
+      }
+
+      &:hover {
+        opacity: .8;
+        top: -8px;
+        @include shadow-down();
+      }
+    }
+
+
+  }
+
+</style>
+
